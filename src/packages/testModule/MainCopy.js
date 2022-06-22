e61db1b1d327a1f9c877311cf9a67cab9f497a24
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2022-06-22 21:14:43 +0200

    test packages added for customConfig

---
+++ src/packages/testModule/MainCopy.js
@@ -0,0 +1,13 @@
+// Ext.define('testModule.Main', {
+//     extend: 'testModule.base.view.Main',
+//     requires: ['testModule.x'],
+//
+//     alias: ['widget.testModuleMain'],
+//
+//     initComponent: function () {
+//         this.x = Ext.create('testModule.x');
+//         this.callParent(arguments);
+//     },
+//
+//     foo: Ext.create()
+// });
