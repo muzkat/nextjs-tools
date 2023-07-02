3a49c8f2153493cf729c09f38dbe86790f740108
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2023-07-02 20:41:59 +0200

    rename clean to emptyFolder

--- utils/build.js
+++ utils/build.js
@@ -1,9 +1,9 @@
 const {rmSync} = require('fs'),
     {log} = require('./log');
 
-const clean = function (folder = 'build') {
+const emptyFolder = function (folder = 'build') {
     log('DELETING BUILD FOLDER');
     rmSync('./' + folder, {recursive: true, force: true});
 }
 
-module.exports = {clean};
+module.exports = {emptyFolder};
